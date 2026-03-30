ebd9d6bf4853c0a32a63724028285735142602c3
suta007 <mr.suta@gmail.com>
2026-03-30 13:24:46 +0700

    esp

--- EfHub.lua
+++ EfHub.lua
@@ -1,9 +1,10 @@
+local game = game :: any
 local PlaceId = game.PlaceId
 
 if PlaceId == 123456789 then -- ไอดีเกม Garden Horizons
-    loadstring(game:HttpGet("https://github.com/suta007/EfHub/releases/latest/download/EfHub_GardenHorizons.lua"))()
+	loadstring(game:HttpGet("https://github.com/suta007/EfHub/releases/latest/download/EfHub_GardenHorizons.lua"))()
 elseif PlaceId == 987654321 then -- ไอดีเกม Grow A Garden
-    loadstring(game:HttpGet("https://github.com/suta007/EfHub/releases/latest/download/EfHub_GrowAGarden.lua"))()
+	loadstring(game:HttpGet("https://github.com/suta007/EfHub/releases/latest/download/EfHub_GrowAGarden.lua"))()
 else
-    game.Players.LocalPlayer:Kick("EfHub ยังไม่รองรับเกมนี้ครับ!")
+	game.Players.LocalPlayer:Kick("EfHub ยังไม่รองรับเกมนี้ครับ!")
 end
